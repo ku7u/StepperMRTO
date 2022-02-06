08dd2f7bda4e4ef14811d00b70ff020914c2fa1a
Ku7u <george.hofmann@gmail.com>
2022-02-06 10:38:43 -0800

    Reconfigure pins, add comments tinker with blink

--- Stepper.ino
+++ Stepper.ino
@@ -5,35 +5,34 @@
  The code has no way of knowing the actual position of the stepper and doesn't need to. It attempts to drive
  the stepper in a known direction, assumes that the move was successful and reports it as such.
 
- Device can be configured by the user using a menu (TBD). If more than one stepper is attached
- then device must be pulled from socket to establish serial communication due to loading of D0 and D1 pins
- by the stepper common connection. Pins will be reassigned to fix this issue (TBD).
+ Device can be configured in place by the user using a menu (TBD). A phone or tablet running a serial app can be
+ used. Or the device can be removed and attached to a computer. Device must be removed from socket when downloading the entire program. 
 
 
   Pin assignments:
   Revised for D13 as output not input. D13 is pulled low by internal LED attached to it.
-   A0 (13) stepper 1 switch
-   A1 (14) stepper 2 switch
-   A2 (15) stepper 3 switch
-   A3 (16) stepper 4 switch
-   A4 (23)  LED mux red - connect to cathode of all red LED, active low
-   A5 (24)  LED mux green - connect to cathode of all green LED, active low
-   A6 (25)  A6 is analog input only
-   A7 (26)  A7 is analog input only
-   D0  (1)  A+ common           // The two pins D0, D1 are normally used for serial comm. configMode will set them as such
-   D1  (2)  B+ common
-   D2  (5)  A- stepper 1
-   D3  (6)  B- stepper 1
-   D4  (7)  A- stepper 2
-   D5  (8)  B- stepper 2
-   D6  (9)  A- stepper 3
-   D7  (10) B- stepper 3
-   D8  (11) A- stepper 4
-   D9  (12) B- stepper 4
-   D10 (19)  stepper 1 R/G LED - connect to anode of both R and G for device 0, active high
-   D11 (20)  stepper 2 R/G LED - connect to anode of both R and G for device 1, active high
-   D12 (21)  stepper 3 R/G LED - connect to anode of both R and G for device 2, active high
-   D13 (22)  stepper 4 R/G LED - connect to anode of both R and G for device 3, active high
+   A0  stepper 1 switch
+   A1  stepper 2 switch
+   A2  stepper 3 switch
+   A3  stepper 4 switch
+   A4  LED mux red - connect to cathode of all red LED, active low, also serial in config mode          
+   A5  LED mux green - connect to cathode of all green LED, active low, also serial in config mode
+   A6  A6 is analog input only
+   A7  A7 is analog input only
+   D0  A+ common, serial pin when configuring
+   D1  B+ common, serial pin when configuring
+   D2  A- stepper 1
+   D3  B- stepper 1
+   D4  A- stepper 2
+   D5  B- stepper 2
+   D6  A- stepper 3
+   D7  B- stepper 3
+   D8  A- stepper 4
+   D9  B- stepper 4
+   D10 stepper 1 R/G LED - connect to anode of both R and G for device 0, active high
+   D11 stepper 2 R/G LED - connect to anode of both R and G for device 1, active high
+   D12 stepper 3 R/G LED - connect to anode of both R and G for device 2, active high
+   D13 stepper 4 R/G LED - connect to anode of both R and G for device 3, active high
    */
 
 #include <Arduino.h>
@@ -44,7 +43,7 @@ const int stepsPerRevolution = 20; // number of steps per revolution
 bool direction = true;             // when true travels in one direction, when false the other
 unsigned long lastStartTime = 0;
 unsigned long timeStamp;
-bool configMode = false; // on reset, test pin A6 high to set configuration mode, else normal mode
+bool configMode = false; // on reset, test switch 0 low to set configuration mode, else normal mode
 
 // motor pins
 const int APlusPin = 0;
@@ -59,14 +58,12 @@ const int AMinus4Pin = 8;
 const int BMinus4Pin = 9;
 
 // switch pins
-//  const int switchPin[4] = {10, 11, 12, 13};
 const int switchPin[4] = {A0, A1, A2, A3};
 
 // LED pins
 const int ledRedMuxPin = A4;
 const int ledGreenMuxPin = A5;
-// const int ledPin[] = {A0, A1, A2, A3};
-const int ledPin[] = {10, 11, 12, 13};
+const int ledPin[4] = {10, 11, 12, 13};
 
 // create an array of steppers with common A+ and B+ connections
 StepperMRTO myStepper[] =
@@ -102,23 +99,23 @@ void setup()
   }
 
   // set the rotational speed using rpm as parameter, defaults to 1000 rpm
-  //  myStepper[0].setSpeed(1000);
+    myStepper[3].setSpeed(500);
 
   // configure the direction
   // also can reverse the wires on one coil instead
   // if reversed is set to true the device will pull the throwbar toward itself, true is the default?
-  myStepper[0].setReversed(false);
+  myStepper[3].setReversed(true);
 
   // limit the torque
   // this defines the maximum time in microseconds that current will be allowed to flow in each step
   // the default is 1800, a larger number provides more torque but consumes more current
   // at 1000 rpm the step length is 3000 ms for a 20 step/revolution motor
-  myStepper[0].setTorqueLimit(1500);
+  myStepper[3].setTorqueLimit(1200);
 
   // set the length of the stroke
   // default is 700 steps which should be enough for all turnouts
   // it could be made less through experimentation
-  myStepper[0].setStrokeSteps(450);
+  myStepper[3].setStrokeSteps(400);
 }
 
 void loop()
@@ -136,13 +133,16 @@ void loop()
 
   // the runSteppers method must be called repeatedly
   runSteppers();
+
+  // display status on LEDs
+  setLEDs();
 }
 
 void checkSwitches()
 {
   for (int i = 0; i < numSteppers; i++)
   {
-    // if corresponding stepper is in ready state or is running just skip it (debounce)
+    // if corresponding stepper is in ready state or is running just skip it (debounces the switch)
     if ((digitalRead(switchPin[i]) == LOW) && (!myStepper[i].getRunState()) && (!myStepper[i].getReadyState()))
     {
       switch (myStepper[i].getLastCommanded())
@@ -182,7 +182,7 @@ void setLEDs() // not complete
 
   lastTimeStamp = millis();
 
-  if (millis() - lastBlink > 500) // TBA
+  if (millis() - lastBlink > 100) // TBA
   {
     blinkState = !blinkState;
     lastBlink = millis();
