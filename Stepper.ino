d10ad0e7762be175d940cc01597baae81907620c
Ku7u <george.hofmann@gmail.com>
2022-02-07 18:41:35 -0800

    Add config menu, eeprom storage

--- Stepper.ino
+++ Stepper.ino
@@ -15,8 +15,8 @@
    A1  stepper 2 switch
    A2  stepper 3 switch
    A3  stepper 4 switch
-   A4  LED mux red - connect to cathode of all red LED, active low, also serial in config mode
-   A5  LED mux green - connect to cathode of all green LED, active low, also serial in config mode
+   A4  LED mux red - connect to cathode of all red LED, active low
+   A5  LED mux green - connect to cathode of all green LED, active low
    A6  A6 is analog input only
    A7  A7 is analog input only
    D0  A+ common, serial pin when configuring
@@ -36,6 +36,7 @@
    */
 
 #include <Arduino.h>
+#include <EEPROM.h>
 #include "StepperMRTO.h"
 
 const int numSteppers = 4;
@@ -72,8 +73,18 @@ StepperMRTO myStepper[] =
      StepperMRTO(stepsPerRevolution, APlusPin, AMinus3Pin, BPlusPin, BMinus3Pin),
      StepperMRTO(stepsPerRevolution, APlusPin, AMinus4Pin, BPlusPin, BMinus4Pin)};
 
+uint16_t const NOMINAL_SPEED = 1000;
+uint16_t const NOMINAL_STROKE = 500;
+uint16_t const NOMINAL_TORQUE_INTERVAL = 1500;
+
+bool notSet[] = {true, true, true, true};
+
 void setup()
 {
+  uint16_t initVal;
+  uint16_t eepVal;
+  uint8_t eepByte;
+
   // switches
   for (int i = 0; i < numSteppers; i++)
     pinMode(switchPin[i], INPUT_PULLUP);
@@ -91,38 +102,63 @@ void setup()
 
   // to trigger menu display, hold down switch 0 while resetting the device - reset when done with menu
   if (digitalRead(switchPin[0]) == LOW)
-  {
-    // do the menu thing TBA
     configMode = true;
-    Serial.begin(9600);
-    return;
+
+  // check for initialization
+  EEPROM.get(0x200, initVal);
+  if (initVal != 666)
+  {
+    EEPROM.put(0x200, 666); // put the magic number 666 at a lofty EEPROM address to indicate initialized state of EEPROM
+    for (int i = 0; i < numSteppers; i++)
+    {
+      eepByte = NOMINAL_SPEED / 10;   // nominal value for speed divided by 10 to make bytesize
+      EEPROM.write((4 * i), eepByte); // speed
+
+      eepByte = NOMINAL_STROKE / 10;
+      EEPROM.write((4 * i) + 1, eepByte); // strokesteps
+
+      eepByte = NOMINAL_TORQUE_INTERVAL / 10;
+      EEPROM.write((4 * i) + 2, eepByte); // torque limit
+
+      EEPROM.write((4 * i) + 3, 0); // reversed = false
+    }
   }
 
-  // set the rotational speed using rpm as parameter, defaults to 1000 rpm
-  myStepper[3].setSpeed(500);
+  for (int i = 0; i < numSteppers; i++)
+  {
+    // read the stored values for speed, throw, torque and reversed
+    // send those values to the stepper objects
 
-  // configure the direction
-  // also can reverse the wires on one coil instead
-  // if reversed is set to true the device will pull the throwbar toward itself, true is the default?
-  myStepper[3].setReversed(true);
+    // set the rotational speed using rpm as parameter, defaults to 1000 rpm
+    eepVal = 10 * EEPROM.read(4 * i);
+    myStepper[i].setSpeed(eepVal);
 
-  // limit the torque
-  // this defines the maximum time in microseconds that current will be allowed to flow in each step
-  // the default is 1800, a larger number provides more torque but consumes more current
-  // at 1000 rpm the step length is 3000 ms for a 20 step/revolution motor
-  myStepper[3].setTorqueLimit(1200);
+    // set the length of the stroke
+    // default is 700 steps which should be enough for all turnouts
+    // it could be made less through experimentation
+    eepVal = 10 * EEPROM.read((4 * i) + 1);
+    myStepper[i].setStrokeSteps(eepVal);
 
-  // set the length of the stroke
-  // default is 700 steps which should be enough for all turnouts
-  // it could be made less through experimentation
-  myStepper[3].setStrokeSteps(400);
+    // limit the torque
+    // this defines the maximum time in microseconds that current will be allowed to flow in each step
+    // the default is 1500, a larger number provides more torque but consumes more current
+    // at 1000 rpm the step length is 3000 ms for a 20 step/revolution motor
+    eepVal = 10 * EEPROM.read((4 * i) + 2);
+    myStepper[i].setTorqueLimit(eepVal);
+
+    // configure the direction
+    // also can reverse the wires on one coil instead
+    // if reversed is set to true the device will pull the throwbar toward itself, false is the default
+    myStepper[i].setReversed(EEPROM.read((4 * i) + 3));
+  }
 }
 
 void loop()
 {
   if (configMode)
   {
-    // TBA show a menu and do some configuration
+    Serial.begin(115200); // TBD this should be set to require rebooting possibly
+    configure();
     configMode = false;
     Serial.end();
     return;
@@ -145,6 +181,7 @@ void checkSwitches()
     // if corresponding stepper is in ready state or is running just skip it (debounces the switch)
     if ((digitalRead(switchPin[i]) == LOW) && (!myStepper[i].getRunState()) && (!myStepper[i].getReadyState()))
     {
+      notSet[i] = false; // turn off blinking seen at startup
       switch (myStepper[i].getLastCommanded())
       {
       case 0:
@@ -194,13 +231,12 @@ void setLEDs() // not complete
     digitalWrite(ledRedMuxPin, HIGH);
   }
 
-  if (myStepper[currentLED].getRunState()) // TBD might want to add ready state to this
+  if (myStepper[currentLED].getRunState() || myStepper[currentLED].getReadyState() || notSet[currentLED])
     digitalWrite(ledPin[currentLED], blinkState); // blinking while moving or TBD ready to move
   else
     digitalWrite(ledPin[currentLED], HIGH); // common anodes
 
-  currentLED++;
-  if (currentLED == 4)
+  if (++currentLED == 4)
     currentLED = 0;
 }
 
@@ -221,6 +257,220 @@ void runSteppers()
       // the first one we find that is ready we set to run and then exit
       myStepper[i].run();
       return;
+    }
+  }
+}
+
+/*****************************************************************************/
+void showMenu()
+
+{
+  Serial.println(F("\nMain menu"));
+  Serial.println(F(" Enter: "));
+  Serial.println(F(" 'S' - Set speed"));
+  Serial.println(F(" 'T' - Set throw"));
+  Serial.println(F(" 'F' - Set force limit"));
+  Serial.println(F(" 'D' - Set direction"));
+  // Serial.println(F(" 'P' - Print status"));
+
+  Serial.println(F("\n Enter 'R' to return to run mode"));
+}
+
+/*****************************************************************************/
+void configure()
+
+{
+  uint16_t devID;
+  uint8_t eepByte;
+
+  while (true)
+  {
+    showMenu();
+
+    switch (getUpperChar())
+    {
+    case 'S':
+      Serial.println(F("Speed (rpm, valid value 1 - 2000, default = 1000)"));
+      Serial.println(F("Enter device (1 - 4) or 0 for all"));
+      devID = getNumber(0, 4);
+      Serial.print(F("Enter value for "));
+      if (devID == 0)
+        Serial.println(F("all devices"));
+      else
+      {
+        Serial.print(F("device "));
+        Serial.println(devID);
+      }
+      eepByte = getNumber(1, 2000)/10;
+      if (devID == 0)
+        for (int i = 0; i < numSteppers; i++)
+        {
+          EEPROM.write(4 * i, eepByte);
+          myStepper[i].setSpeed(eepByte * 10);
+        }
+      else
+      {
+        EEPROM.write(4 * (devID - 1), eepByte);
+        myStepper[devID].setSpeed(eepByte * 10);
+      }
+      break;
+
+    case 'T':
+      Serial.println(F("Throw (steps, valid value 200 - 800, default = 600, 1000 steps = 7mm)"));
+      Serial.println(F("Enter device (1 - 4) or 0 for all"));
+      devID = getNumber(0, 4);
+      Serial.print(F("Enter value for "));
+      if (devID == 0)
+        Serial.println(F("all devices"));
+      else
+      {
+        Serial.print(F("device "));
+        Serial.println(devID);
+      }
+      eepByte = getNumber(200, 800)/10;
+      if (devID == 0)
+        for (int i = 0; i < numSteppers; i++)
+
+        {
+          EEPROM.write((4 * i) + 1, eepByte);
+          myStepper[i].setStrokeSteps(eepByte * 10);
+        }
+      else
+      {
+        EEPROM.write(4 * (devID - 1) + 1, eepByte);
+        myStepper[devID].setStrokeSteps(eepByte * 10);
+      }
+      break;
+
+    case 'F':
+      Serial.println(F("Force (microseconds, valid value 800 - 2000, default = 1500)"));
+      Serial.println(F("Enter device (1 - 4) or 0 for all"));
+      devID = getNumber(0, 4);
+      Serial.print(F("Enter value for "));
+      if (devID == 0)
+        Serial.println(F("all devices"));
+      else
+      {
+        Serial.print(F("device "));
+        Serial.println(devID);
+      }
+      eepByte = getNumber(800, 2000)/10;
+      if (devID == 0)
+        for (int i = 0; i < numSteppers; i++)
+        {
+          EEPROM.write((4 * i) + 2, eepByte);
+          myStepper[i].setTorqueLimit(eepByte * 10);
+        }
+      else
+      {
+        EEPROM.write(4 * (devID - 1) + 2, eepByte);
+        myStepper[devID].setTorqueLimit(eepByte * 10);
+      }
+      break;
+
+    case 'D':
+      Serial.println(F("Direction (0 or 1, 1 reverses the normal direction of throw"));
+      Serial.println(F("Enter device (1 - 4) or 0 for all"));
+      devID = getNumber(0, 4);
+      Serial.print(F("Enter value for "));
+      if (devID == 0)
+        Serial.println(F("all devices"));
+      else
+      {
+        Serial.print(F("device "));
+        Serial.println(devID);
+      }
+      eepByte = getNumber(0, 1);
+      if (devID == 0)
+        for (int i = 0; i < numSteppers; i++)
+        {
+          EEPROM.write(4 * i + 3, eepByte);
+          myStepper[i].setReversed(eepByte);
+        }
+      else
+      {
+        EEPROM.write(4 * (devID - 1) + 3, eepByte);
+        myStepper[devID].setReversed(eepByte);
+      }
+      break;
+
+    case 'P':
+      Serial.println(F("Print current configuration")); // TBD
+      break;
+
+    case 'R':
+      return;
+
+    default:
+      break;
+    }
+  }
+}
+
+/*****************************************************************************/
+char getUpperChar()
+/*****************************************************************************/
+{
+  // gets one character and converts to upper case, clears input buffer of C/R and newline
+  char _myChar;
+
+  while (true)
+  {
+    if (Serial.available() > 0)
+    {
+      delay(5);
+      _myChar = Serial.read();
+
+      if (_myChar > 96)
+        _myChar -= 32;
+
+      flushSerialIn();
+      return _myChar;
+    }
+  }
+}
+
+/*****************************************************************************/
+void flushSerialIn(void)
+/*****************************************************************************/
+{
+  while (Serial.available() > 0)
+  { // clear the buffer
+    delay(5);
+    Serial.read();
+  }
+}
+
+/*****************************************************************************/
+int getNumber(int min, int max)
+/*****************************************************************************/
+{
+  int _inNumber;
+
+  while (true)
+  {
+    if (Serial.available() > 0)
+    {
+      _inNumber = Serial.parseInt();
+      // Serial.println(_inNumber);
+
+      // if (_inNumber == 0)
+      // {
+      //   // Serial.println(F("Exiting"));
+      //   flushSerialIn();
+      //   return -1;
+      // }
+      // else
+      if ((_inNumber < min) || (_inNumber > max))
+      {
+        Serial.println(F("Out of range, reenter"));
+        flushSerialIn();
+      }
+      else
+      {
+        flushSerialIn();
+        return _inNumber;
+      }
     }
   }
 }
